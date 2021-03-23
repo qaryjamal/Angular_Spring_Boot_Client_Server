0073ea2d02c12d3c1b3d88a34a5d68b17b54b8ce
qaryjamal <qaryjamal@gmail.com>
2021-03-24 00:07:17 +0500

    ss

---
+++ springboot_backend/src/main/java/net/javaguides/model/Employee.java
@@ -0,0 +1,51 @@
+package net.javaguides.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+	@Id@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String address;
+	private String email;
+	
+	
+	public Employee() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
